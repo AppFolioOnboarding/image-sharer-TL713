b9436651fb1e3cf817a6d4eba81c67ff2c2a3f40
Qihan Liu <qihan.liu@appfolio.com>
2019-12-11 15:30:28 -0800

    ql - add test for imag submission page and show page

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -4,4 +4,20 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'new_image_path returns a 200 response' do
+    get new_image_path
+    assert_response :ok
+  end
+
+  test 'link saved in database' do
+    @image = Image.new(link: 'http://www.example.com', title: 'image')
+    assert @image.save
+  end
+
+  test 'image_path returns a 200 response' do
+    @image = Image.new(link: 'http://www.example.com', title: 'image')
+    @image.save
+    get image_path(@image)
+    assert_response :ok
+  end
 end
