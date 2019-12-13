445cd2fe1e3ef4c27094732107a7efab0e7a1429
Qihan Liu <qihan.liu@appfolio.com>
2019-12-13 10:09:28 -0800

    ql - add unit test to test error message in model test

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -4,20 +4,24 @@ class ImageTest < ActiveSupport::TestCase
   test 'valid image' do
     image = Image.new(title: 'name', link: 'http://www.google.com')
     assert image.valid?
+    assert_not image.errors.any?
   end
 
   test 'link cannot be empty' do
     image = Image.new(title: 'name')
     assert_not image.valid?
+    assert_equal "Link can't be blank", image.errors.full_messages[0]
   end
 
   test 'title cannot be empty' do
     image = Image.new(link: 'http://www.google.com')
     assert_not image.valid?
+    assert_equal "Title can't be blank", image.errors.full_messages[0]
   end
 
   test 'url need to be valid' do
     image = Image.new(link: '123456', title: 'false url ')
     assert_not image.valid?
+    assert_equal 'Link is not a valid URL', image.errors.full_messages[0]
   end
 end
